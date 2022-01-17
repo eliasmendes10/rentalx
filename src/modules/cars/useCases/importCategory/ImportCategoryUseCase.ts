615585a01575e2be0e548fde71e42d10ba661f3a
Elias <eliasmdes@gmail.com>
2022-01-16 21:36:36 -0300

    refactor

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,5 @@
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { parse as csvParse } from "csv-parse";
-import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 import fs from "fs";
 import { inject, injectable } from "tsyringe";
 
