9fcb256de48589ddb17e4088d2a7ab782b3ceb98
Elias <eliasmdes@gmail.com>
2022-01-16 22:42:12 -0300

    Refatoração 2

--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import multer from "multer";
-import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
-import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateAvatar/UpdateUserAvatarController";
-import uploadConfig from "../config/upload";
+
+import uploadConfig from "@config/upload";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,11 +3,11 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import { router } from "./routes";
 import swaggerUI from "swagger-ui-express";
-import swaggerFile from "./swagger.json";
+import swaggerFile from "../../../swagger.json";
 
-import "./database";
+import "@shared/infra/typeorm";
 import "@shared/container";
-import { AppError } from "@errors/AppError";
+import { AppError } from "@shared/errors/AppError";
 
 const app = express();
 
